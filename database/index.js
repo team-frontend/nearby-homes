61bfa534f487dd94f0b969a7240207876016e978
sukm <sung.kim572@gmail.com>
2018-11-13 14:17:17 -0800

    Modify db index

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('neighborhood', 'root', '1', {
+const db = new Sequelize('neighborhood', 'root', '', {
   dialect: 'mysql',
   operatorsAliases: false,
   define: {
@@ -9,17 +9,3 @@ const db = new Sequelize('neighborhood', 'root', '1', {
 });
 
 module.exports = db;
-
-
-// const Sequelize = require('sequelize');
-
-// const db = new Sequelize('nearbyHomes', 'skim7', 'gorfldprxj#', {
-//   host: 'hr-fcc-nearby-homes.cppzgmwnb7qi.us-east-1.rds.amazonaws.com',
-//   dialect: 'mysql',
-//   operatorsAliases: false,
-//   define: {
-//     timestamps: false,
-//   },
-// });
-
-// module.exports = db;
